c756999b3e81606ce3748467d4e15bd680098e83
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-23 01:47:17 +0200

    first Row

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:video_training_app/utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +11,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 30),
+        child: Column(
+          children: [
+            // 1st Row Title Icons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Training',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w700,
+                    color: AppColor.homePageTitle,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios_outlined,
+                      color: AppColor.homePageIcons,
+                      size: 16,
+                    ),
+                    SizedBox(width: 10),
+                    Icon(
+                      Icons.calendar_today_outlined,
+                      color: AppColor.homePageIcons,
+                      size: 16,
+                    ),
+                    SizedBox(width: 10),
+                    Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: AppColor.homePageIcons,
+                      size: 16,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+          ],
+        ),
+      ),
+    );
   }
 }
