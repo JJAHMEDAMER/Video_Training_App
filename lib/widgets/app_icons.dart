e3e38c47dd283ecf87c199fcf2c88831373553b6
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-23 21:26:59 +0200

    app Icon widget

---
+++ lib/widgets/app_icons.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:video_training_app/utils/colors.dart';
+
+class AppIcon extends StatelessWidget {
+  // class variables
+  final IconData varIcon;
+  final double varSize;
+  final Color varColor;
+
+  //
+  const AppIcon({
+    super.key,
+    required this.varIcon,
+    this.varSize = 16,
+    this.varColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      varIcon,
+      size: varSize,
+      color: varColor == Colors.white ? AppColor.veryLightBlueIcon : varColor,
+    );
+  }
+}
