794921c0b13b8332fe662e9f2c3252f2ff4712e6
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-23 21:31:47 +0200

    First Row Icons

--- lib/pages/video_page.dart
+++ lib/pages/video_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_training_app/utils/colors.dart';
+import 'package:video_training_app/widgets/app_icons.dart';
 
 class VideoPage extends StatefulWidget {
   const VideoPage({super.key});
@@ -10,6 +12,40 @@ class VideoPage extends StatefulWidget {
 class _VideoPageState extends State<VideoPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          left: 15,
+          right: 15,
+          top: 20,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              AppColor.gradientFirst,
+              AppColor.gradientSecond,
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+        child: Column(children: [
+
+          // 1st row icons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AppIcon(varIcon: Icons.arrow_back_ios),
+              AppIcon(varIcon: Icons.info_outline)
+            ],
+          ),
+          SizedBox(height: 15),
+
+          // 2nd Row
+          
+        
+        ]),
+      ),
+    );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static Color homePageBackground = const Color(0xFFfbfcff);
+  //gradients
   static Color gradientFirst = const Color(0xff0f17ad);
   static Color gradientSecond = const Color(0xFF6985e8);
+
+  //Home Page
   static Color homePageTitle = const Color(0xFF302f51);
   static Color homePageSubtitle = const Color(0xFF414160);
   static Color homePageDetail = const Color(0xFF6588f4);
@@ -11,6 +14,8 @@ class AppColor {
   static Color homePageContainerTextSmall = const Color(0xFFf4f5fd);
   static Color homePageContainerTextBig = const Color(0xFFffffff);
   static Color homePagePlanColor = const Color(0xFFa2a2b1);
+
+  // Second Page
   static Color secondPageTopIconColor = const Color(0xFFb7bce8);
   static Color secondPageTitleColor = const Color(0xFFfefeff);
   static Color secondPageContainerGradient1stColor = const Color(0xFF5564d8);
@@ -19,4 +24,7 @@ class AppColor {
   static Color loopColor = const Color(0xFF6d8dea);
   static Color setsColor = const Color(0xFF9999a9);
   static Color circuitsColor = const Color(0xFF2f2f51);
+
+  // More Colors
+  static Color veryLightBlueIcon = const Color(0xFFb7bce8);
 }
