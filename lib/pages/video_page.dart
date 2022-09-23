3ffe49f78ff2ed93aee071aaa9b3591e8c0ba65a
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-24 01:37:37 +0200

    Use two containers layout

--- lib/pages/video_page.dart
+++ lib/pages/video_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:video_training_app/utils/colors.dart';
 import 'package:video_training_app/widgets/app_icons.dart';
+import 'package:video_training_app/widgets/app_text.dart';
 
 class VideoPage extends StatefulWidget {
   const VideoPage({super.key});
@@ -10,15 +13,26 @@ class VideoPage extends StatefulWidget {
 }
 
 class _VideoPageState extends State<VideoPage> {
+  List videoData = [];
+
+  _jsonExtract() {
+    DefaultAssetBundle.of(context)
+        .loadString('json/videoinfo.json')
+        .then((value) {
+      return videoData = json.decode(value);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _jsonExtract();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(
-          left: 15,
-          right: 15,
-          top: 20,
-        ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
@@ -31,86 +45,100 @@ class _VideoPageState extends State<VideoPage> {
         ),
         child: Column(
           children: [
-            // 1st row icons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AppIcon(varIcon: Icons.arrow_back_ios),
-                AppIcon(varIcon: Icons.info_outline)
-              ],
-            ),
-            SizedBox(height: 15),
-
-            // 2nd Row Text
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Legs Toning and Glutes Workout',
-                    style: TextStyle(
-                        color: AppColor.bigText,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-
-            // 3rd Row Info Bar
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7),
-                    color: AppColor.bigText.withOpacity(0.15),
-                  ),
-                  child: Row(
+            // 1st Container
+            Container(
+              padding: EdgeInsets.only(
+                left: 15,
+                right: 15,
+                top: 20,
+              ),
+              child: Column(
+                children: [
+                  // 1st row icons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      AppIcon(
-                        varIcon: Icons.timer,
-                        varSize: 14,
-                      ),
-                      Text(
-                        ' 68 min',
-                        style: TextStyle(
-                            color: AppColor.bigText,
-                            fontSize: 11,
-                            fontWeight: FontWeight.w400),
-                      )
+                      AppIcon(varIcon: Icons.arrow_back_ios),
+                      AppIcon(varIcon: Icons.info_outline)
                     ],
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7),
-                    color: AppColor.bigText.withOpacity(0.15),
-                  ),
-                  child: Row(
+                  SizedBox(height: 15),
+
+                  // 2nd Row Text
+                  Row(
                     children: [
-                      AppIcon(
-                        varIcon: Icons.handyman,
-                        varSize: 14,
+                      Expanded(
+                        child: Text(
+                          'Legs Toning and Glutes Workout',
+                          style: TextStyle(
+                              color: AppColor.bigText,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w400),
+                        ),
                       ),
-                      Text(
-                        ' Resistant band, Kettlebell',
-                        style: TextStyle(
-                            color: AppColor.bigText,
-                            fontSize: 11,
-                            fontWeight: FontWeight.w400),
-                      )
                     ],
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
+                  SizedBox(height: 20),
 
-            // 4th Row
+                  // 3rd Row Info Bar
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          color: AppColor.bigText.withOpacity(0.15),
+                        ),
+                        child: Row(
+                          children: [
+                            AppIcon(
+                              varIcon: Icons.timer,
+                              varSize: 14,
+                            ),
+                            Text(
+                              ' 68 min',
+                              style: TextStyle(
+                                  color: AppColor.bigText,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w400),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          color: AppColor.bigText.withOpacity(0.15),
+                        ),
+                        child: Row(
+                          children: [
+                            AppIcon(
+                              varIcon: Icons.handyman,
+                              varSize: 14,
+                            ),
+                            Text(
+                              ' Resistant band, Kettlebell',
+                              style: TextStyle(
+                                  color: AppColor.bigText,
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w400),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+
+            // 2nd Container
+            // Wrap with expanded to fill the possible space
+            
           ],
         ),
       ),
